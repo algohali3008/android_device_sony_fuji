cead0a44f9827a14ac28b5c85c781043c71bee85
paul-xxx <thepaulxxx@gmail.com>
2014-11-20 18:22:54 +0600

    fix build

--- Android.mk
+++ Android.mk
@@ -1,4 +1,3 @@
-ifneq ($(filter hikari, nozomi, aoba,$(TARGET_DEVICE)),)
-LOCAL_PATH := $(call my-dir)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),fuji)
+    include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
