9ce7c233a532426ecac7edfcaccdf1b31016ca33
paul-xxx <thepaulxxx@gmail.com>
2014-12-17 01:11:54 +0600

    update mkfile

--- Android.mk
+++ Android.mk
@@ -1,3 +1,9 @@
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),fuji)
-    include $(call all-subdir-makefiles,$(LOCAL_PATH))
+#
+# Copyright 2014-2015, BPaul
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter fuji,$(TARGET_BOOTLOADER_BOARD_NAME)),)
+include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
